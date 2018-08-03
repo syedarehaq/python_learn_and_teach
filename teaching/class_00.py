cb90e696c94003c81a5b325059abd151bcc7c95c
Syed Arefinul Haque <arefindk@gmail.com>
2018-08-03 14:14:13 -0400

    the oth class for python teaching

---
+++ teaching/class_00.py
@@ -0,0 +1,58 @@
+## lets start using python3 from here.
+## https://github.com/jerry-git/learn-python3
+
+## Lets print a string, a number
+## lets print the lower case of a string, check if isalpha, isalphanum, isnum
+## Assign something to variable
+## see the type of the variable
+## get help about a function (e.g. .lower function of the string we lowercased before)
+
+## learn casting, check a number if it is alphanumeric or numeric only
+## cast a isnum string into an integer, cast a floaitng point number into into string and add it with another alpha string
+## and print it
+
+## List object
+## empty list creation
+## append something
+## create a whole list of four different objects
+## show the list of lists later
+
+## Lets first try looping a list
+# http://htmlpreview.github.io/?https://github.com/jerry-git/learn-python3/blob/master/notebooks/beginner/html/for_loops.html
+# then try break, continue, enumerate
+
+## Now use range to create a list (need to have the concept of casting something to a list)
+## then print those numbers iterating through that list
+## print numbers 0,10
+## print numbers 1,10
+## print numbers 1,3,5,7,9 without the step function in range
+## print numbers 1,3,5,7,9 with step function in range
+## then print those numbers just
+## iterating through the range
+
+## Now iterate through those numbers but do not print the numbers itself, just soemthing else
+## like the numer + hello world
+
+## Lets define a function that prints something 10 times
+def print_dormammu_10_times():
+    for i in range(10):
+        print("Dormammu")
+print_hello_world_10_times()
+"""
+Now take a parameter then print it 10 times:
+
+Now take a string paramter and an integer parameter and print the string paramter that many times the integer parameter says
+
+Write a function that prints from 1 upto that number e.g. 100 prints 1.....99,100
+
+Now take an integer paramter then square it and print it
+
+Now take an integer paramter then square it and return it. You don't have to print it
+
+Now take this returned squared integer from a function and print it the integer times
+"""
+
+# What's next?
+# http://codingbat.com/prob/p120546
+## File reading writing
+## dictionary
